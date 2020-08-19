bda9bbcea4053c4e0c6ca20f08ea8136702da155
kukulim <beckon@interia.pl>
2020-08-19 14:43:08 +0200

    add monster

--- src/views/MainBoard.vue
+++ src/views/MainBoard.vue
@@ -1,10 +1,48 @@
 <template>
   <div class="container">
-    nowa gra w ninije
+    <div>
+      <div><p>Stage: {{stage}}</p><p>World: {{world}}</p></div>
+      <div>{{monster.health}}</div>
+      <img src="../assets/logo.png" alt="" @click="hit()">
+    </div>
     </div>
 </template>
 
 <script>
+export default {
+  name: 'Ninija',
+  data() {
+    return {
+      monster:{
+        basehealth:10,
+        health:10,
+        bosshealth:30,
+        bosspower:2,
+      },
+      hero:{
+        lvl:1,
+        hitpower:1,
+        weapon:2,
+      },
+      stage:1,
+      world:1,
+    };
+  },
+    methods:{
+    hit(){
+      this.monster.health-=(this.hero.hitpower+this.hero.weapon);
+      if(this.monster.health <= 0){
+        this.stage++;
+        this.monster.health = this.monster.basehealth + this.stage;
+      }
+      if(this.stage==10){
+        this.monster.health=this.monster.bosshealth*this.monster.bosspower;
+        this.monster.bosspower++;
+        this.stage=1;
+      }
+    },
+  }
+}
 </script>
 
 <style></style>
