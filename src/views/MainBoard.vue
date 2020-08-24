50554346029025f2a6278390434cf5ce8c6c2a0b
kukulim <beckon@interia.pl>
2020-08-24 17:38:36 +0200

    dodany gold

--- src/views/MainBoard.vue
+++ src/views/MainBoard.vue
@@ -2,58 +2,85 @@
   <div class="container">
     <div class="row mt-5">
       <div class="col">
+        <img src="../images/gold.png" alt="cap" class="goldimg" />
         <div>
-          <p>Gold: {{ hero.gold }}</p>
+          <div class="goldvalue mt-2">: {{ hero.gold }}</div>
         </div>
-        
-        <img src="../images/armor/cap.png" alt="cap" class="capimg">
-        <img src="../images/armor/katana.png" alt="cap" class="katanaimg">
-        <img src="../images/armor/shuriken.png" alt="cap" class="shurikenimg">
-        <img src="../images/armor/sai.png" alt="cap" class="saiimg">
-        <img src="../images/armor/knife.png" alt="cap" class="knifeimg">
+        <div class="mt-5">
+          <img src="../images/armor/cap.png" alt="cap" class="capimg" />
+          <img src="../images/armor/katana.png" alt="cap" class="katanaimg" />
+          <img
+            src="../images/armor/shuriken.png"
+            alt="cap"
+            class="shurikenimg"
+          />
+          <img src="../images/armor/sai.png" alt="cap" class="saiimg" />
+          <img src="../images/armor/knife.png" alt="cap" class="knifeimg" />
 
-        <img src="../images/hero.png" class="heroimg">
-        <p>hero lvl: {{ hero.lvl }}</p>
-        <div class="proggresbar">
-        <b-progress
-          :value="hero.exp"
-          :max="hero.maxlvlvalue"
-          variant='success'
-          class="mb-3"
-        ></b-progress>
-        </div>
-        <p>skills:</p>
-        <div class="row">
-          <div class="col">
-            <div @click="usefury()" :class="activefury()"><img src="../images/skills/a.png" alt="" class="btn btn-info skillsbutton"></div>
+          <img src="../images/hero.png" class="heroimg" />
+          <p>hero lvl: {{ hero.lvl }}</p>
+          <div class="proggresbar">
+            <b-progress
+              :value="hero.exp"
+              :max="hero.maxlvlvalue"
+              variant="success"
+              class="mb-3"
+            ></b-progress>
           </div>
-          <div class="col">
-            <div @click="usepowerhit()" :class="activepowerhit()"><img src="../images/skills/b.png" alt="" class="btn btn-danger skillsbutton"></div>
-          </div>
-          <div class="col">
-            <div @click="useshurikenwind()" :class="activeshurikenwind()"><img src="../images/skills/c.png" alt="" class="btn btn-success skillsbutton"></div>
+          <p>skills:</p>
+          <div class="row">
+            <div class="col">
+              <div @click="usefury()" :class="activefury()">
+                <img
+                  src="../images/skills/a.png"
+                  alt=""
+                  class="btn btn-info skillsbutton"
+                />
+              </div>
+            </div>
+            <div class="col">
+              <div @click="usepowerhit()" :class="activepowerhit()">
+                <img
+                  src="../images/skills/b.png"
+                  alt=""
+                  class="btn btn-danger skillsbutton"
+                />
+              </div>
+            </div>
+            <div class="col">
+              <div @click="useshurikenwind()" :class="activeshurikenwind()">
+                <img
+                  src="../images/skills/c.png"
+                  alt=""
+                  class="btn btn-success skillsbutton"
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      <div></div>
       <div class="col">
         <div>
           <p>Stage: {{ stage }} World: {{ world }}</p>
         </div>
-        <div class="monsterhealthproggresbar mt-5"> 
-                <b-progress
-          :value="monster.health"
-          :max="monster.currentmonsterhealth"
-          variant='danger'
-          class="mb-3"
-        ></b-progress>
-        <div v-show="elementVisible" class="hideElement hitpozition">-{{monster.health-monster.health}}</div>
+        <div class="monsterhealthproggresbar mt-5">
+          <b-progress
+            :value="monster.health"
+            :max="monster.currentmonsterhealth"
+            variant="danger"
+            class="mb-3"
+          ></b-progress>
+          <div v-show="elementVisible" class="hideElement hitpozition">
+            -{{ monster.health - monster.health }}
+          </div>
         </div>
-        <img 
-        :src="require(`@/images/${monster.type[0]}.png`)" 
-        alt="" @click="hit()" 
-        class="monsterimg"
-        v-bind:class="getClass()" />
+        <img
+          :src="require(`@/images/${monster.type[0]}.png`)"
+          alt=""
+          @click="hit()"
+          class="monsterimg"
+          v-bind:class="getClass()"
+        />
       </div>
     </div>
   </div>
@@ -67,11 +94,11 @@ export default {
       monster: {
         basehealth: 30,
         health: 30,
-        currentmonsterhealth:30,
+        currentmonsterhealth: 30,
         bosshealth: 30,
         bosspower: 2,
         worldbosspower: 2,
-        type:["A","B","C","D","E","F","G"]
+        type: ["A", "B", "C", "D", "E", "F", "G"],
       },
       hero: {
         lvl: 1,
@@ -83,37 +110,39 @@ export default {
       },
       stage: 1,
       world: 1,
-      elementVisible:false,
-      skills:{
-        fury:{dps:2, isactive:false},
-        powerhit:{dps:50, isactive:false}
-      }
+      elementVisible: false,
+      skills: {
+        fury: { dps: 2, isactive: false },
+        powerhit: { dps: 50, isactive: false },
+      },
     };
   },
   methods: {
     hit() {
-      this.elementVisible=true;
-      setTimeout(() => this.elementVisible = false, 100);
+      this.elementVisible = true;
+      setTimeout(() => (this.elementVisible = false), 100);
       this.monster.health -=
         this.hero.hitpower + this.hero.weapon + 2 * this.hero.lvl;
-        if(this.skills.fury.isactive==true){
-          this.monster.health -=((this.hero.hitpower + this.hero.weapon + 2 * this.hero.lvl)*this.skills.fury.dps);
-        }
-        if(this.skills.powerhit.isactive==true){
-          this.monster.health-=this.skills.powerhit.dps;
-          this.skills.powerhit.isactive=false;
-        }
+      if (this.skills.fury.isactive == true) {
+        this.monster.health -=
+          (this.hero.hitpower + this.hero.weapon + 2 * this.hero.lvl) *
+          this.skills.fury.dps;
+      }
+      if (this.skills.powerhit.isactive == true) {
+        this.monster.health -= this.skills.powerhit.dps;
+        this.skills.powerhit.isactive = false;
+      }
       if (this.monster.health <= 0) {
         this.stage++;
         this.shuffleMosnsters();
-        this.hero.gold=parseInt(1.1*this.stage*this.world);
+        this.hero.gold = parseInt(1.1 * this.stage * this.world);
         this.hero.exp += 10;
         this.monster.health = this.monster.basehealth + this.stage * this.world;
-        this.monster.currentmonsterhealth=this.monster.health
+        this.monster.currentmonsterhealth = this.monster.health;
         if (this.stage == 25 || this.stage == 50 || this.stage == 75) {
           this.monster.health =
             this.monster.bosshealth * this.monster.bosspower;
-            this.monster.currentmonsterhealth=this.monster.health
+          this.monster.currentmonsterhealth = this.monster.health;
           this.monster.bosspower++;
         }
         if (this.stage == 100) {
@@ -121,7 +150,7 @@ export default {
             this.monster.bosshealth *
             this.monster.bosspower *
             this.monster.worldbosspower;
-            this.monster.currentmonsterhealth=this.monster.health
+          this.monster.currentmonsterhealth = this.monster.health;
           this.monster.worldbosspower++;
         }
       }
@@ -135,39 +164,43 @@ export default {
         this.hero.maxlvlvalue = parseInt(this.hero.maxlvlvalue * 1.1);
       }
     },
-    usefury(){
-      this.skills.fury.isactive=true;
-      setTimeout(() => this.skills.fury.isactive = false, 30000);
+    usefury() {
+      this.skills.fury.isactive = true;
+      setTimeout(() => (this.skills.fury.isactive = false), 30000);
     },
-    usepowerhit(){
-      this.skills.powerhit.isactive=true;
+    usepowerhit() {
+      this.skills.powerhit.isactive = true;
     },
-    useshurikenwind(){
-        var myTimer = setInterval(() => this.hit(), 500);
-        setTimeout(() =>  clearInterval(myTimer), 30000);
+    useshurikenwind() {
+      var myTimer = setInterval(() => this.hit(), 500);
+      setTimeout(() => clearInterval(myTimer), 30000);
     },
     shuffleMosnsters() {
       for (let i = this.monster.type.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
-        [this.monster.type[i], this.monster.type[j]] = [this.monster.type[j], this.monster.type[i]];
+        [this.monster.type[i], this.monster.type[j]] = [
+          this.monster.type[j],
+          this.monster.type[i],
+        ];
       }
     },
-    getClass(){
-      if(this.stage == 25 || this.stage == 50 || this.stage == 75) return "miniboss";
-      if (this.stage == 100) return "bigboss"
+    getClass() {
+      if (this.stage == 25 || this.stage == 50 || this.stage == 75)
+        return "miniboss";
+      if (this.stage == 100) return "bigboss";
     },
-    activefury(){
-      if(this.hero.lvl>=10) return "";
+    activefury() {
+      if (this.hero.lvl >= 10) return "";
       else return "disableddiv";
     },
-    activepowerhit(){
-      if(this.hero.lvl>=20) return "";
+    activepowerhit() {
+      if (this.hero.lvl >= 20) return "";
       else return "disableddiv";
     },
-    activeshurikenwind(){
-      if(this.hero.lvl>=30) return "";
+    activeshurikenwind() {
+      if (this.hero.lvl >= 30) return "";
       else return "disableddiv";
-    }
+    },
   },
 };
 </script>
