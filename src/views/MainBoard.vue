c2063c74b06561d0f975114ad162e4720912fc02
kukulim <beckon@interia.pl>
2020-08-25 14:10:43 +0200

    style

--- src/views/MainBoard.vue
+++ src/views/MainBoard.vue
@@ -1,49 +1,102 @@
 <template>
   <div class="container">
+    <!--HERO-->
+
     <div class="row mt-5">
       <div class="col">
-        <img src="../images/gold.png" alt="cap" class="goldimg"/>
+        <img src="../images/gold.png" alt="cap" class="goldimg" />
         <div>
           <div class="goldvalue mt-2">: {{ hero.gold }}</div>
         </div>
         <div class="mt-5">
-          <img src="../images/armor/cap.png" alt="cap" class="capimg" @click="buycap()" v-bind:class="[{ 'opaticyimgbuyed': equipment.cap.status },'opaticyimg']"/>
-          <div class="capequipnemtvalue" >
-            <span>cost: {{equipment.cap.cost}}</span><br>
-            <span>lvl: {{equipment.cap.lvl}}</span><br>
-            <span>dps: {{equipment.cap.dps}}</span>
+          <img
+            src="../images/armor/cap.png"
+            alt="cap"
+            class="capimg"
+            @click="buycap()"
+            v-bind:class="[
+              { opaticyimgbuyed: equipment.cap.status },
+              'opaticyimg',
+            ]"
+          />
+          <div class="capequipnemtvalue">
+            <span>cost: {{ equipment.cap.cost }}</span
+            ><br />
+            <span>lvl: {{ equipment.cap.lvl }}</span
+            ><br />
+            <span>dps: {{ equipment.cap.dps }}</span>
           </div>
-          <img src="../images/armor/katana.png" alt="cap" class="katanaimg opaticyimg" />
+          <img
+            src="../images/armor/katana.png"
+            alt="cap"
+            class="katanaimg opaticyimg"
+            @click="buykatana()"
+            v-bind:class="[
+              { opaticyimgbuyed: equipment.katana.status },
+              'opaticyimg',
+            ]"
+          />
           <div class="katanaequipnemtvalue">
-            <span>cost: {{equipment.katana.cost}}</span><br>
-            <span>lvl: {{equipment.katana.lvl}}</span><br>
-            <span>dps: {{equipment.katana.dps}}</span>
+            <span>cost: {{ equipment.katana.cost }}</span
+            ><br />
+            <span>lvl: {{ equipment.katana.lvl }}</span
+            ><br />
+            <span>dps: {{ equipment.katana.dps }}</span>
           </div>
           <img
             src="../images/armor/shuriken.png"
             alt="cap"
             class="shurikenimg opaticyimg"
+            @click="buyshuriken()"
+            v-bind:class="[
+              { opaticyimgbuyed: equipment.shuriken.status },
+              'opaticyimg',
+            ]"
           />
           <div class="shurikenequipnemtvalue">
-            <span>cost: {{equipment.shuriken.cost}}</span><br>
-            <span>lvl: {{equipment.shuriken.lvl}}</span><br>
-            <span>dps: {{equipment.shuriken.dps}}</span>
+            <span>cost: {{ equipment.shuriken.cost }}</span
+            ><br />
+            <span>lvl: {{ equipment.shuriken.lvl }}</span
+            ><br />
+            <span>dps: {{ equipment.shuriken.dps }}</span>
           </div>
-          <img src="../images/armor/sai.png" alt="cap" class="saiimg opaticyimg" />
+          <img
+            src="../images/armor/sai.png"
+            alt="cap"
+            class="saiimg opaticyimg"
+            @click="buysai()"
+            v-bind:class="[
+              { opaticyimgbuyed: equipment.sai.status },
+              'opaticyimg',
+            ]"
+          />
           <div class="saiequipnemtvalue">
-            <span>cost: {{equipment.sai.cost}}</span><br>
-            <span>lvl: {{equipment.sai.lvl}}</span><br>
-            <span>dps: {{equipment.sai.dps}}</span>
+            <span>cost: {{ equipment.sai.cost }}</span
+            ><br />
+            <span>lvl: {{ equipment.sai.lvl }}</span
+            ><br />
+            <span>dps: {{ equipment.sai.dps }}</span>
           </div>
-          <img src="../images/armor/knife.png" alt="cap" class="knifeimg opaticyimg" />
+          <img
+            src="../images/armor/knife.png"
+            alt="cap"
+            class="knifeimg opaticyimg"
+            @click="buyknife()"
+            v-bind:class="[
+              { opaticyimgbuyed: equipment.knife.status },
+              'opaticyimg',
+            ]"
+          />
           <div class="knifeequipnemtvalue">
-            <span>cost: {{equipment.knife.cost}}</span><br>
-            <span>lvl: {{equipment.knife.lvl}}</span><br>
-            <span>dps: {{equipment.knife.dps}}</span>
+            <span>cost: {{ equipment.knife.cost }}</span
+            ><br />
+            <span>lvl: {{ equipment.knife.lvl }}</span
+            ><br />
+            <span>dps: {{ equipment.knife.dps }}</span>
           </div>
 
           <img src="../images/hero.png" class="heroimg" />
-          <p>hero lvl: {{ hero.lvl }}</p>
+          <p>LVL: {{ hero.lvl }}</p>
           <div class="proggresbar">
             <b-progress
               :value="hero.exp"
@@ -52,7 +105,10 @@
               class="mb-3"
             ></b-progress>
           </div>
-          <p>skills:</p>
+
+          <!--SKILLS-->
+
+          <p>SKILLS:</p>
           <div class="row">
             <div class="col">
               <div @click="usefury()" :class="activefury()">
@@ -61,6 +117,7 @@
                   alt=""
                   class="btn btn-info skillsbutton"
                 />
+                <p>FURY</p>
               </div>
             </div>
             <div class="col">
@@ -70,6 +127,7 @@
                   alt=""
                   class="btn btn-danger skillsbutton"
                 />
+                <p>POWER HIT</p>
               </div>
             </div>
             <div class="col">
@@ -79,14 +137,18 @@
                   alt=""
                   class="btn btn-success skillsbutton"
                 />
+                <p>SHURIKEN WIND</p>
               </div>
             </div>
           </div>
         </div>
       </div>
+
+      <!--Monster Side-->
+
       <div class="col">
         <div>
-          <p>Stage: {{ stage }} World: {{ world }}</p>
+          <p>STAGE: {{ stage }} WORLD: {{ world }}</p>
         </div>
         <div class="monsterhealthproggresbar mt-5">
           <b-progress
@@ -122,7 +184,7 @@ export default {
         currentmonsterhealth: 80,
         bosshealth: 500,
         bosspower: 2,
-        worldbosspower: 2,
+        worldbosspower: 4,
         type: ["A", "B", "C", "D", "E", "F", "G"],
       },
       hero: {
@@ -130,18 +192,18 @@ export default {
         exp: 0,
         maxlvlvalue: 100,
         gold: 0,
-        hitpower: 0
+        hitpower: 0,
       },
       skills: {
         fury: { dps: 2, isactive: false },
         powerhit: { dps: 50, isactive: false },
       },
-      equipment:{
-        cap:{lvl:0, dps:1, cost:5, status:false},
-        katana:{lvl:0, dps:2, cost:10},
-        shuriken:{lvl:0, dps:3, cost:20},
-        sai:{lvl:0, dps:4, cost:30},
-        knife:{lvl:0, dps:5, cost:50},
+      equipment: {
+        cap: { lvl: 0, dps: 1, cost: 5, status: false },
+        katana: { lvl: 0, dps: 2, cost: 10, status: false },
+        shuriken: { lvl: 0, dps: 3, cost: 20, status: false },
+        sai: { lvl: 0, dps: 4, cost: 30, status: false },
+        knife: { lvl: 0, dps: 5, cost: 50, status: false },
       },
       stage: 1,
       world: 1,
@@ -153,16 +215,21 @@ export default {
       this.elementVisible = true;
       setTimeout(() => (this.elementVisible = false), 100);
 
-      this.hero.hitpower = this.hero.lvl+this.equipment.cap.dps+this.equipment.katana.dps+this.equipment.shuriken.dps+this.equipment.sai.dps+this.equipment.knife.dps;
+      this.hero.hitpower =
+        this.hero.lvl * 2 +
+        this.equipment.cap.dps +
+        this.equipment.katana.dps +
+        this.equipment.shuriken.dps +
+        this.equipment.sai.dps +
+        this.equipment.knife.dps;
 
-      this.monster.health -=this.hero.hitpower
+      this.monster.health -= this.hero.hitpower;
       if (this.skills.fury.isactive == true) {
-        this.hero.hitpower*=this.skills.fury.dps;
-        this.monster.health -=
-          this.hero.hitpower;
+        this.hero.hitpower *= this.skills.fury.dps;
+        this.monster.health -= this.hero.hitpower;
       }
       if (this.skills.powerhit.isactive == true) {
-        this.hero.hitpower = this.skills.powerhit.dps*this.hero.lvl
+        this.hero.hitpower = this.skills.powerhit.dps * this.hero.lvl;
         this.monster.health -= this.hero.hitpower;
         this.skills.powerhit.isactive = false;
       }
@@ -172,7 +239,7 @@ export default {
         this.hero.gold += parseInt(1.1 * this.stage * this.world);
         this.hero.exp += 10;
 
-        this.monster.basehealth +=this.stage * this.world * 2
+        this.monster.basehealth += this.stage * this.world * 2;
         this.monster.health = this.monster.basehealth;
 
         this.monster.currentmonsterhealth = this.monster.health;
@@ -188,7 +255,7 @@ export default {
             this.monster.bosspower *
             this.monster.worldbosspower;
           this.monster.currentmonsterhealth = this.monster.health;
-          this.monster.worldbosspower++;
+          this.monster.worldbosspower += 4;
         }
       }
       if (this.stage == 101) {
@@ -227,26 +294,62 @@ export default {
       if (this.stage == 100) return "bigboss";
     },
     activefury() {
-      if (this.hero.lvl >= 10) return "";
+      if (this.hero.lvl >= 5) return "";
       else return "disableddiv";
     },
     activepowerhit() {
-      if (this.hero.lvl >= 20) return "";
+      if (this.hero.lvl >= 10) return "";
       else return "disableddiv";
     },
     activeshurikenwind() {
-      if (this.hero.lvl >= 30) return "";
+      if (this.hero.lvl >= 20) return "";
       else return "disableddiv";
     },
-    buycap(){
-      if(this.hero.gold>=this.equipment.cap.cost){
-        this.hero.gold-=this.equipment.cap.cost;
-      this.equipment.cap.dps++;
-      this.equipment.cap.lvl++;
-      this.equipment.cap.cost+=89;
-      this.equipment.cap.status=true;
+    buycap() {
+      if (this.hero.gold >= this.equipment.cap.cost) {
+        this.hero.gold -= this.equipment.cap.cost;
+        this.equipment.cap.dps++;
+        this.equipment.cap.lvl++;
+        this.equipment.cap.cost += 89;
+        this.equipment.cap.status = true;
       }
-    }
+    },
+    buykatana() {
+      if (this.hero.gold >= this.equipment.katana.cost) {
+        this.hero.gold -= this.equipment.katana.cost;
+        this.equipment.katana.dps += 2;
+        this.equipment.katana.lvl++;
+        this.equipment.katana.cost += 99;
+        this.equipment.katana.status = true;
+      }
+    },
+    buyshuriken() {
+      if (this.hero.gold >= this.equipment.shuriken.cost) {
+        this.hero.gold -= this.equipment.shuriken.cost;
+        this.equipment.shuriken.dps += 3;
+        this.equipment.shuriken.lvl++;
+        this.equipment.shuriken.cost += 109;
+        this.equipment.shuriken.status = true;
+      }
+    },
+    buysai() {
+      if (this.hero.gold >= this.equipment.sai.cost) {
+        this.hero.gold -= this.equipment.sai.cost;
+        this.equipment.sai.dps += 4;
+        this.equipment.sai.lvl++;
+        this.equipment.sai.cost += 149;
+        this.equipment.sai.status = true;
+      }
+    },
+    buyknife() {
+      if (this.hero.gold >= this.equipment.knife.cost) {
+        this.hero.gold -= this.equipment.knife.cost;
+        this.equipment.knife.dps += 6;
+        this.equipment.knife.lvl++;
+        this.equipment.knife.cost += 189;
+        this.equipment.knife.status = true;
+      }
+    },
   },
 };
 </script>
